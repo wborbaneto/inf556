3c6852fb4aab7f33ac9e7f30f76252619fdbd345
wborbaneto <wborbaneto@gmail.com>
2019-11-08 16:47:10 -0300

    v1.0

--- install.sh
+++ install.sh
@@ -1,1 +1,22 @@
-apt-get install python-pip
+# cleaning
+
+fuser -k 6653/tcp
+rm /var/lib/apt/lists/lock
+rm /var/cache/apt/archives/lock
+rm /var/lib/dpkg/lock
+
+
+#
+apt-get --yes --force-yes install python-pip
+pip install webob
+pip install tinyrpc==1.0.2
+pip install routes
+pip install ovs
+pip install oslo.config
+pip install msgpack
+pip install eventlet==0.18.2
+
+
+# ryu
+cd ~/; git clone git://github.com/osrg/ryu.git
+cd ryu; python ./setup.py install
